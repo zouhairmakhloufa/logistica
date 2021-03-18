0d526e2578c54d2ef8851cd558fa37b0690b3c94
zouhairmakhloufa <zouhairmakhloufa11@gmail.com>
2021-03-18 16:53:20 +0100

    new commit Map

--- src/Modules/Auth/mapInformation/DraggableMarker.tsx
+++ src/Modules/Auth/mapInformation/DraggableMarker.tsx
@@ -2,45 +2,40 @@ import { useState, useMemo, useRef } from 'react';
 import { Marker, useMapEvents } from 'react-leaflet';
 import L from "leaflet";
 
-const center = {
-    lat: 35.5030347,
-    lng: 11.055748099999999
-  }
-  const icon = L.icon({
-    iconSize: [25, 41],
-    iconAnchor: [10, 41],
-    popupAnchor: [2, -40],
-    iconUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png",
-    shadowUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-shadow.png"
-  });
-  function DraggableMarker() {
-    const [position, setPosition] = useState(center)
-    const markerRef = useRef(null)
-    const eventHandlers = useMemo(
-      () => ({
-        dragend() {
-          const marker: any = markerRef.current
-          if (marker != null) {
-            setPosition(marker.getLatLng() )
-          }
-        },
-      }),
-      [],
-    )
-    useMapEvents({
-      click(e) {
-        setPosition(e.latlng)
+const icon = L.icon({
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+  iconUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-shadow.png"
+});
+function DraggableMarker(props: any) {
+  const [position, setPosition] = useState(props.position)
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker: any = markerRef.current
+        if (marker != null) {
+          setPosition(marker.getLatLng())
+        }
       },
-    })
-    return (
-
-   <Marker
-        draggable={true}
-        eventHandlers={eventHandlers}
-        position={position}
-        icon={icon}
-        ref={markerRef}>
-      </Marker>
-    )
-  }
-  export default DraggableMarker;	
+    }),
+    [],
+  )
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng)
+    },
+  })
+  return (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      icon={icon}
+      ref={markerRef}>
+    </Marker>
+  )
+}
+export default DraggableMarker;
--- src/Modules/Auth/mapInformation/mapInformation.tsx
+++ src/Modules/Auth/mapInformation/mapInformation.tsx
@@ -3,42 +3,38 @@ import "leaflet/dist/leaflet.css";
 import "./mapInformation.scss";
 import { Button } from 'antd';
 import DraggableMarker from "./DraggableMarker";
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
-const center = {
-  lat: 35.5030347,
-  lng: 11.055748099999999
-}
 function MapInformation() {
-  const [position, setPosition] = useState({lat: 0, lng: 0});
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      debugger
-      setPosition({ 
-        lat:  position.coords.latitude,
-        lng: position.coords.longitude
-      })
-    });
-  }, [])
-  
+  const position = { lat: 35.5030347, lng: 11.055748099999999 };
+  const [source, setSource] = useState({ lat: 35.5030347, lng: 11.055748099999999 })
+
   return (
     <div className="contentImage">
       <MapContainer
-        center={center}
-        zoom={9}
-        style={{ height: "100vh" }} >
+        center={position}
+        zoom={4}
+        style={{ height: "100vh" }}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          
-        <DraggableMarker />
+
+        <DraggableMarker position={source} />
 
       </MapContainer>
       <div className="auth">
         <h1>Pick me up From </h1>
-        <input placeholder="Search Places Start" style={{ width: "80%" }}  />
         <Button className="button" type="primary" htmlType="submit"
-          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> set-pickup </Button>
+          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> choose your adress Source </Button>
+        <br></br>
+        <Button className="button" type="primary" htmlType="submit"
+          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> choose your adress destination </Button>
+
+        <br></br>
+        <br></br>
+        <Button className="buttonNext" type="primary" htmlType="submit"
+          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> Next </Button>
       </div>
     </div>
   );
