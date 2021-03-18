9c81e25dc11f330045b30c3a3688588d50dadc48
saber <saberfathallah7@gmail.com>
2021-03-18 21:51:42 +0100

    use position

--- src/Modules/Auth/mapInformation/mapInformation.tsx
+++ src/Modules/Auth/mapInformation/mapInformation.tsx
@@ -1,40 +1,62 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./mapInformation.scss";
-import { Button } from 'antd';
+import { Button } from "antd";
 import DraggableMarker from "./DraggableMarker";
 import { useState } from "react";
+import usePosition from "../../../hooks/getPosition";
 
 function MapInformation() {
   const position = { lat: 35.5030347, lng: 11.055748099999999 };
-  const [source, setSource] = useState({ lat: 35.5030347, lng: 11.055748099999999 })
-
+  const [source, setSource] = useState({
+    lat: 35.5030347,
+    lng: 11.055748099999999,
+  });
+  const posss = usePosition();
+  console.log("posss", posss);
   return (
     <div className="contentImage">
-      <MapContainer
-        center={position}
-        zoom={4}
-        style={{ height: "100vh" }}
-      >
+      <MapContainer center={position} zoom={4} style={{ height: "100vh" }}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
         <DraggableMarker position={source} />
-
       </MapContainer>
       <div className="auth">
         <h1>Pick me up From </h1>
-        <Button className="button" type="primary" htmlType="submit"
-          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> choose your adress Source </Button>
+        <Button
+          className="button"
+          type="primary"
+          htmlType="submit"
+          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+        >
+          {" "}
+          choose your adress Source{" "}
+        </Button>
         <br></br>
-        <Button className="button" type="primary" htmlType="submit"
-          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> choose your adress destination </Button>
+        <Button
+          className="button"
+          type="primary"
+          htmlType="submit"
+          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+        >
+          {" "}
+          choose your adress destination{" "}
+        </Button>
 
         <br></br>
         <br></br>
-        <Button className="buttonNext" type="primary" htmlType="submit"
-          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}> Next </Button>
+        <Button
+          className="buttonNext"
+          type="primary"
+          htmlType="submit"
+          style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+        >
+          {" "}
+          Next{" "}
+        </Button>
       </div>
     </div>
   );
