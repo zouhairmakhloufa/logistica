af9abe5f277deea2baeeca37a8794f8f7ff1bb5f
saber <saberfathallah7@gmail.com>
2021-03-19 20:49:28 +0100

    fix code

--- src/Modules/Auth/mapInformation/mapInformation.tsx
+++ src/Modules/Auth/mapInformation/mapInformation.tsx
@@ -3,44 +3,84 @@ import "leaflet/dist/leaflet.css";
 import "./mapInformation.scss";
 import { Button } from "antd";
 import DraggableMarker from "./DraggableMarker";
-import { useState } from "react";
-import usePosition from "../../../hooks/getPosition";
+import { useEffect, useState } from "react";
 
 function MapInformation() {
-  const position = { lat: 35.5030347, lng: 11.055748099999999 };
+  const position: { lat: number; lng: number } = { lat: 0, lng: 0 };
   const [source, setSource] = useState({
     lat: 35.5030347,
     lng: 11.055748099999999,
+    showMarker: false,
   });
-  const posss = usePosition();
-  console.log("posss", posss);
+  const [destination, setDestination] = useState({
+    lat: 35.5030347,
+    lng: 10.055748099999999,
+    showMarker: false,
+  });
+
+  useEffect(() => {
+    const geo = navigator.geolocation;
+    geo.getCurrentPosition((pos) => {
+      if (pos.coords.altitude && pos.coords.longitude) {
+        position.lat = pos.coords.altitude;
+        position.lng = pos.coords.longitude;
+      }
+    });
+  }, [position]);
+
+  let buttonAdressSource;
+  if (source.showMarker) {
+    buttonAdressSource = <DraggableMarker position={source} />;
+  }
+  let buttonAdressDestination;
+  if (destination.showMarker) {
+    buttonAdressDestination = <DraggableMarker position={destination} />;
+  }
   return (
     <div className="contentImage">
-      <MapContainer center={position} zoom={4} style={{ height: "100vh" }}>
+      <MapContainer center={source} zoom={5} style={{ height: "100vh" }}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        <DraggableMarker position={source} />
+        {buttonAdressSource}
+        {buttonAdressDestination}
       </MapContainer>
+
       <div className="auth">
         <h1>Pick me up From </h1>
+
         <Button
           className="button"
           type="primary"
           htmlType="submit"
           style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+          onClick={(e) =>
+            setSource({
+              lat: 35.5030347,
+              lng: 11.055748099999999,
+              showMarker: true,
+            })
+          }
         >
           {" "}
           choose your adress Source{" "}
         </Button>
+
         <br></br>
+
         <Button
           className="button"
           type="primary"
           htmlType="submit"
           style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+          onClick={(e) =>
+            setDestination({
+              lat: 35.5030347,
+              lng: 10.055748099999999,
+              showMarker: true,
+            })
+          }
         >
           {" "}
           choose your adress destination{" "}
@@ -48,14 +88,22 @@ function MapInformation() {
 
         <br></br>
         <br></br>
+
         <Button
           className="buttonNext"
           type="primary"
           htmlType="submit"
           style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+          onClick={(e) =>
+            setDestination({
+              lat: 35.5030347,
+              lng: 10.055748099999999,
+              showMarker: true,
+            })
+          }
         >
           {" "}
-          Next{" "}
+          Add{" "}
         </Button>
       </div>
     </div>
