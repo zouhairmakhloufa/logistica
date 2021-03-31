c9546d983204755039be35aeed3b653645e181fa
saber <saberfathallah7@gmail.com>
2021-03-31 20:05:33 +0200

    code

--- src/Modules/Auth/SignupVeifCode/SignupVeifCode.tsx
+++ src/Modules/Auth/SignupVeifCode/SignupVeifCode.tsx
@@ -1,38 +1,61 @@
-import { Form, Input, Button, } from 'antd';
+import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
 import { useHistory } from "react-router-dom";
-import { LeftSquareOutlined } from "@ant-design/icons"
-//  map filter forEach reduce 
+import { LeftSquareOutlined } from "@ant-design/icons";
+import InputCode from "./InputCode";
+import "./SignupVeifCode.scss";
+//  map filter forEach reduce
 
 const SignupVeifCode = () => {
   const [form] = Form.useForm();
   const history = useHistory();
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="contentImage">
       <img src="/imageAuth/imageAuth.jpg" className="imageAuth" />
       <div className="auth-signup-info">
-        
-        <LeftSquareOutlined onClick={() => history.push("/Auth/signupEmail")} className="ClickRetour" />
-        <h1>Verif code  </h1>
-
-        <Form.Item name="password" label="Code"
+        <LeftSquareOutlined
+          onClick={() => history.push("/Auth/signupEmail")}
+          className="ClickRetour"
+        />
+        <h1>Verif code </h1>
+        <Form.Item
+          name="password"
+          label="Code"
           rules={[
             {
               required: true,
-              message: 'Please input your password!',
+              message: "Please input your password!",
             },
           ]}
           hasFeedback
         >
-          <Input.Password style={{ width: '70%' }} />
+          <Input.Password style={{ width: "70%" }} placeholder="code" />
         </Form.Item>
-        <Form.Item >
-          <Button className="button" type="primary" htmlType="submit"
+        <Form.Item>
+          <Button
+            className="button"
+            type="primary"
+            htmlType="submit"
             style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
-            onClick={() => history.push("/Auth/UserOrDriver")}> Continue </Button>
+            onClick={() => history.push("/Auth/UserOrDriver")}
+          >
+            {" "}
+            Continue{" "}
+          </Button>
         </Form.Item>
+        <InputCode
+          length={4}
+          label="Code Label"
+          loading={loading}
+          onComplete={(code: string) => {
+            console.log("code", code);
+            setLoading(true);
+          }}
+        />{" "}
       </div>
     </div>
-  )
-}
+  );
+};
 export default SignupVeifCode;
