304cd7c68a1ae5a83d8dd0ba236ec2f062ac2401
saber <saberfathallah7@gmail.com>
2021-04-19 17:03:45 +0200

    jjjj

---
+++ src/Routers.tsx
@@ -0,0 +1,76 @@
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import dashbord from "./Modules/dashbord";
+import SignupVeifCode from "./Modules/Auth/SignupVeifCode";
+import SignupDriver from "./driver/components/SignupDriver";
+import SignupUser from "./user/components/Signup";
+import MapInformation from "./Modules/Auth/mapInformation";
+import PoidsAndTaille from "./user/components/PoidsAndTaille";
+import TypeOfCars from "./user/components/TypeOfCars";
+import Service from "./user/components/Service";
+import BookingSuccessful from "./user/components/BookingSuccessful";
+import NoteToDriver from "./user/components/NoteToDriver";
+import RetourRequest from "./user/components/RetourRequest";
+import MenuUser from "./user/components/MenuUser";
+import Info from "./user/components/Info";
+import OrdersUser from "./user/components/OrdersUser";
+import NotificationUser from "./user/components/NotificationUser";
+import SettingUser from "./user/components/SettingUser";
+import PaymentUser from "./user/components/PaymentUser";
+import PaymentMethode from "./user/components/PaymentMethode";
+import AddPaymentMethod from "./user/components/AddPaymentMethod";
+import InfoDetailCurrent from "./user/components/InfoDetailCurrent";
+import MenuDriver from "./driver/components/MenuDriver";
+import HomeDriver from "./driver/components/HomeDriver";
+import SettingDriver from "./driver/components/SettingDriver";
+import Detaille from "./driver/components/Detaille";
+import OrdersDriver from "./driver/components/OrdersDriver";
+import Signin from "./Modules/Auth/Signin";
+import LoginAsUserOrDriver from "./Modules/Auth/LoginAsUserOrDriver";
+import SignUpUserOrDriver from "./Modules/Auth/SignUpUserOrDriver";
+
+function Routers(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Route path="/" exact component={dashbord} />
+        <Route path="/MenuUser" exact component={MenuUser} />
+        <Route path="/MenuDriver" exact component={MenuDriver} />
+        <Route path="/OrdersDriver" exact component={OrdersDriver} />
+        <Route path="/Info" exact component={Info} />
+        <Route path="/InfoDetailCurrent" exact component={InfoDetailCurrent} />
+        <Route path="/OrdersUser" exact component={OrdersUser} />
+        <Route path="/PaymentUser" exact component={PaymentUser} />
+        <Route path="/PaymentMethode" exact component={PaymentMethode} />
+        <Route path="/AddPaymentMethod" exact component={AddPaymentMethod} />
+        <Route path="/NotificationUser" exact component={NotificationUser} />
+        <Route path="/SettingUser" exact component={SettingUser} />
+        <Route path="/SettingDriver" exact component={SettingDriver} />
+        <Route path="/Auth/Signin" exact component={Signin} />
+        <Route path="/Auth/SignupVeifCode" exact component={SignupVeifCode} />
+        <Route
+          path="/Auth/LoginAsUserOrDriver"
+          exact
+          component={LoginAsUserOrDriver}
+        />
+        <Route
+          path="/Auth/SignUpUserOrDriver"
+          exact
+          component={SignUpUserOrDriver}
+        />
+        <Route path="/Auth/SignupUser" exact component={SignupUser} />
+        <Route path="/Auth/SignupDriver" exact component={SignupDriver} />
+        <Route path="/mapInformation" exact component={MapInformation} />
+        <Route path="/PoidsAndTaille" exact component={PoidsAndTaille} />
+        <Route path="/TypeOfCars" exact component={TypeOfCars} />
+        <Route path="/Service" exact component={Service} />
+        <Route path="/BookingSuccessful" exact component={BookingSuccessful} />
+        <Route path="/HomeDriver" exact component={HomeDriver} />
+        <Route path="/Detaille" exact component={Detaille} />
+        <Route path="/NoteToDriver" exact component={NoteToDriver} />
+        <Route path="/RetourRequest" exact component={RetourRequest} />
+      </div>
+    </BrowserRouter>
+  );
+}
+export default Routers;
--- src/driver/components/Detaille/Detaille.tsx
+++ src/driver/components/Detaille/Detaille.tsx
@@ -1,12 +1,13 @@
-import { Button, Timeline , Popconfirm } from "antd";
-import { LeftSquareOutlined } from "@ant-design/icons"
+import { Button, Timeline, Popconfirm } from "antd";
+import { LeftSquareOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router-dom";
-import { DollarOutlined } from "@ant-design/icons"
-import "./Detaille.scss"
+import { DollarOutlined } from "@ant-design/icons";
+
+import "./Detaille.scss";
 
 const data = [
   {
-    title: 'Logistica cars',
+    title: "Logistica cars",
   },
 ];
 const Detaille = () => {
@@ -15,41 +16,48 @@ const Detaille = () => {
     <div className="contentImage">
       <img src="/imageAuth/imageAuth.jpg" className="imageAuthInfo" />
       <div className="InfoBooking">
-        <LeftSquareOutlined onClick={() => history.push("/HomeDriver")} className="ClickRetourDetaille" />
+        <LeftSquareOutlined
+          onClick={() => history.push("/HomeDriver")}
+          className="ClickRetourDetaille"
+        />
 
         <div>
           <h2>Details</h2>
           <Timeline className="timelineinfo">
             <Timeline.Item color="green">6731 Al Ulaya, Al Wurud</Timeline.Item>
-            <Timeline.Item color="green">King Khalid International Airport </Timeline.Item>
+            <Timeline.Item color="green">
+              King Khalid International Airport{" "}
+            </Timeline.Item>
           </Timeline>
         </div>
         <div className="paymentMethode">
           <h2 className="TextPaymentMethode"> Payment Methode</h2>
-          <DollarOutlined style={{ fontSize: '22px', color: '#08c' }} /> <Button className="ButtonCach" type="primary" htmlType="submit"
-          > Cach{" "}
+          <DollarOutlined style={{ fontSize: "22px", color: "#08c" }} />{" "}
+          <Button className="ButtonCach" type="primary" htmlType="submit">
+            {" "}
+            Cach{" "}
           </Button>
         </div>
 
         <div className="noteToDriver">
-          <h2 > Note To Driver</h2>
+          <h2> Note To Driver</h2>
           <h4>E.g I am located on the right side of</h4>
         </div>
 
         <h2> Pricing</h2>
-        <div style={{ display: "flex" }} className="Pricing" >
+        <div style={{ display: "flex" }} className="Pricing">
           <h4 className="pricingDeliveryequipe"> Delivery Service</h4>
           <h4>5 dt</h4>
         </div>
-        <div style={{ display: "flex" }} className="Pricing" >
+        <div style={{ display: "flex" }} className="Pricing">
           <h4 className="pricingBoardingequipe"> Boarding Service</h4>
           <h4>5 dt</h4>
         </div>
-        <div style={{ display: "flex" }} className="Pricing" >
+        <div style={{ display: "flex" }} className="Pricing">
           <h4 className="pricingShippingequipe"> Shipping Service</h4>
           <h4>5 dt</h4>
         </div>
-        <div style={{ display: "flex" }} className="Pricing" >
+        <div style={{ display: "flex" }} className="Pricing">
           <h4 className="pricingequipe"> Cortoon Service</h4>
           <h4>5 dt</h4>
         </div>
@@ -58,20 +66,51 @@ const Detaille = () => {
           <h2 className="texttottalamount">Total Amount</h2>
           <h4 className="TotalAmount20">20 dt</h4>
         </div>
-        <Button
+
+        <Popconfirm
+          title={"are you sure ?"}
+          onConfirm={() => history.push("/HomeDriver")}
+        >
+          <Button
+            className="buttonMap"
+            type="primary"
+            style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
+          >
+            Accept offer
+          </Button>
+        </Popconfirm>
+        <Popconfirm
+          title={"are you sure ?"}
+          onConfirm={() => history.push("/HomeDriver")}
+        >
+          <Button
+            className="buttonMap"
+            type="primary"
+            style={{ background: "red", borderColor: "red" }}
+          >
+            Cancel offer
+          </Button>
+        </Popconfirm>
+        {/* <Button
           className="button-Accept-Offer"
           type="primary"
           htmlType="submit"
           style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
-          onClick={() => history.push("/HomeDriver")}> Accept offer{" "}
+          onClick={() => history.push("/HomeDriver")}
+        >
+          {" "}
+          Accept offer
         </Button>
         <Button
           className="button-cancel-Offer"
           type="primary"
           htmlType="submit"
           style={{ background: "red", borderColor: "red" }}
-          onClick={() => history.push("/HomeDriver")}> Cancel offer{" "}
-        </Button>
+          onClick={() => history.push("/HomeDriver")}
+        >
+          {" "}
+          Cancel offer{" "}
+        </Button> */}
       </div>
     </div>
   );
