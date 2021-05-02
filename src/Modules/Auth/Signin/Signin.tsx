c52a081c9309c2b03a77fa15dd0a8bd89e6958f5
Saber FATHALLAH <saberfathallah@ewm082.local>
2021-05-02 23:46:17 +0200

    login

--- src/Modules/Auth/Signin/Signin.tsx
+++ src/Modules/Auth/Signin/Signin.tsx
@@ -1,12 +1,27 @@
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button } from "antd";
 import { useHistory } from "react-router-dom";
-import { LeftSquareOutlined,MailOutlined,LockOutlined } from "@ant-design/icons"
-import "./Signin.scss"
-//  map filter forEach reduce 
+import {
+  LeftSquareOutlined,
+  MailOutlined,
+  LockOutlined,
+} from "@ant-design/icons";
+import axios from "axios";
+
+import "./Signin.scss";
+//  map filter forEach reduce
 const Signin = () => {
   const history = useHistory();
-  const onFinish = (values: any) => {
-    console.log("Success:", values);
+  const onFinish = async (values: any) => {
+    const user = await axios.post("http://localhost:5000/User/login", {
+      email: values.email,
+      password: values.password,
+    });
+    console.log("user", user);
+
+    //
+
+    //
+    //
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -17,7 +32,10 @@ const Signin = () => {
     <div className="contentImage">
       <img src="/imageAuth/imageAuth.jpg" className="imageAuth" />
       <div className="auth-signup-info">
-        <LeftSquareOutlined onClick={() => history.push("/")} className="ClickRetourSignin" />
+        <LeftSquareOutlined
+          onClick={() => history.push("/")}
+          className="ClickRetourSignin"
+        />
         <h1>Sign-in </h1>
         <h4>Welcome back! </h4>
         <Form
@@ -28,47 +46,65 @@ const Signin = () => {
         >
           <Form.Item
             name="email"
-    
             rules={[
               {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
+                type: "email",
+                message: "The input is not valid E-mail!",
               },
               {
                 required: true,
-                message: 'Please input your E-mail!',
+                message: "Please input your E-mail!",
               },
             ]}
           >
-            <Input placeholder="Enter your Email" style={{ width: "80%" }} prefix={<MailOutlined /> }/>
+            <Input
+              placeholder="Enter your Email"
+              style={{ width: "80%" }}
+              prefix={<MailOutlined />}
+            />
           </Form.Item>
 
           <Form.Item
             name="password"
-
             rules={[
               {
                 required: true,
-                message: 'Please input your password!',
+                message: "Please input your password!",
               },
             ]}
             hasFeedback
           >
-            <Input.Password placeholder="Enter your Password" style={{ width: "80%" }} prefix={<LockOutlined /> }/>
+            <Input.Password
+              placeholder="Enter your Password"
+              style={{ width: "80%" }}
+              prefix={<LockOutlined />}
+            />
           </Form.Item>
 
-          <div style={{ display: "flex" }} >
-            <h5 style={{ marginRight: "50px" }} className="NotYetRegister">Not yet register ?</h5>
-            <a href="/Auth/SignUpUserOrDriver" className="Signup"> Sign up</a>
+          <div style={{ display: "flex" }}>
+            <h5 style={{ marginRight: "50px" }} className="NotYetRegister">
+              Not yet register ?
+            </h5>
+            <a href="/Auth/SignUpUserOrDriver" className="Signup">
+              {" "}
+              Sign up
+            </a>
           </div>
-          <Form.Item >
-            <Button className="button " type="primary" htmlType="submit"
+          <Form.Item>
+            <Button
+              className="button "
+              type="primary"
+              htmlType="submit"
               style={{ background: "#66CDAA", borderColor: "#66CDAA" }}
-              onClick={() => history.push("/Auth/LoginAsUserOrDriver")}> Sign in </Button>
+              value="Submit"
+            >
+              {" "}
+              Sign in{" "}
+            </Button>
           </Form.Item>
         </Form>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Signin;
