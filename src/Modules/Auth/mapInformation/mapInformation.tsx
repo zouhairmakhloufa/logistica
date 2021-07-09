91c5b22584d83aa3f52ecf5bd989bff5c6e691a4
zouhairmakhloufa <zouhairmakhloufa11@gmail.com>
2021-07-09 03:56:27 +0100

    fix code

--- src/Modules/Auth/mapInformation/mapInformation.tsx
+++ src/Modules/Auth/mapInformation/mapInformation.tsx
@@ -43,6 +43,23 @@ function MapInformation() {
     { source: "Mahdia", destination: "Kef", distance: 163 },
     { source: "Mahdia", destination: "Nabeul", distance: 170 },
     { source: "Mahdia", destination: "Ariana", distance: 220 },
+    { source: "Mahdia", destination: "Tozeur", distance: 290 },
+    { source: "Mahdia", destination: "Béja", distance: 163 },
+    { source: "Mahdia", destination: "Bizerte", distance: 276 },
+    { source: "Mahdia", destination: "Ben Arous", distance: 200 },
+    { source: "Mahdia", destination: "Gabès", distance: 264 },
+    { source: "Mahdia", destination: "Gafsa", distance: 325 },
+    { source: "Mahdia", destination: "Jendouba", distance: 360 },
+    { source: "Mahdia", destination: "Kairouan", distance: 101 },
+    { source: "Mahdia", destination: "Kébili", distance: 362 },
+    { source: "Mahdia", destination: "Béja", distance: 314 },
+    { source: "Mahdia", destination: "Kef", distance: 275 },
+    { source: "Mahdia", destination: "Manouba", distance: 217 },
+    { source: "Mahdia", destination: "Médenine", distance: 337 },
+    { source: "Mahdia", destination: "Sidi Bouzid", distance: 190 },
+    { source: "Mahdia", destination: "Siliana", distance: 204 },
+    { source: "Mahdia", destination: "Tataouine", distance: 391 },
+
     { source: "Sfax", destination: "Sousse", distance: 132 },
     { source: "Sfax", destination: "Monastir", distance: 134 },
     { source: "Sfax", destination: "Tunis", distance: 271 },
@@ -50,34 +67,116 @@ function MapInformation() {
     { source: "Sfax", destination: "Ariana", distance: 278 },
     { source: "Sfax", destination: "Zaghouan", distance: 261 },
     { source: "Sfax", destination: "Kef", distance: 291 },
+    { source: "Sfax", destination: "Ben Arous", distance: 280 },
+    { source: "Sfax", destination: "Gabès", distance: 161 },
+    { source: "Sfax", destination: "Gafsa", distance: 200 },
+    { source: "Sfax", destination: "Jendouba", distance: 418 },
+    { source: "Sfax", destination: "Kairouan", distance: 135 },
+    { source: "Sfax", destination: "Kébili", distance: 259 },
+    { source: "Sfax", destination: "Béja", distance: 374 },
+    { source: "Sfax", destination: "Bizerte", distance: 336 },
+    { source: "Sfax", destination: "Manouba", distance: 277 },
+    { source: "Sfax", destination: "Médenine", distance: 234 },
+    { source: "Sfax", destination: "Sidi Bouzid", distance: 135 },
+    { source: "Sfax", destination: "Siliana", distance: 235 },
+    { source: "Sfax", destination: "Tataouine", distance: 288 },
+    { source: "Sfax", destination: "Tozeur", distance: 293 },
+
     { source: "Tunis", destination: "Sousse", distance: 147 },
     { source: "Tunis", destination: "Nabeul", distance: 68 },
     { source: "Tunis", destination: "Zaghouan", distance: 56 },
     { source: "Tunis", destination: "Tozeur", distance: 454 },
     { source: "Tunis", destination: "Monastir", distance: 168 },
+    { source: "Tunis", destination: "Ariana", distance: 13 },
+    { source: "Tunis", destination: "Ben Arous", distance: 11 },
+    { source: "Tunis", destination: "Bizerte", distance: 70 },
+    { source: "Tunis", destination: "Gabès", distance: 414 },
+    { source: "Tunis", destination: "Gafsa", distance: 362 },
+    { source: "Tunis", destination: "Jendouba", distance: 155 },
+    { source: "Tunis", destination: "Kairouan", distance: 161 },
+    { source: "Tunis", destination: "Kébili", distance: 513 },
+    { source: "Tunis", destination: "Béja", distance: 111 },
+    { source: "Tunis", destination: "Kef", distance: 167 },
+    { source: "Tunis", destination: "Manouba", distance: 10 },
+    { source: "Tunis", destination: "Médenine", distance: 488 },
+
     { source: "Sousse", destination: "Monastir", distance: 21 },
     { source: "Sousse", destination: "Nabeul", distance: 108 },
-    { source: "Sousse", destination: "Ariana", distance: 158 },
+    { source: "Sousse", destination: "Ariana", distance: 158 }, 
     { source: "Sousse", destination: "Zaghouan", distance: 102 },
+    { source: "Sousse", destination: "Kef", distance: 253 },
+    { source: "Sousse", destination: "Sidi Bouzid", distance: 175 },
+    { source: "Sousse", destination: "Kébili", distance: 380 },
+    { source: "Sousse", destination: "Ben Arous", distance: 161 },
+    { source: "Sousse", destination: "Kébili", distance: 380 },
+    { source: "Sousse", destination: "Bizerte", distance: 256 },
+    { source: "Sousse", destination: "Gabès", distance: 282 },
+    { source: "Sousse", destination: "Gafsa", distance: 274 },
+    { source: "Sousse", destination: "Jendouba", distance: 320 },
+    { source: "Sousse", destination: "Kairouan", distance: 68 },
+    { source: "Sousse", destination: "Kébili", distance: 421 },
+    { source: "Sousse", destination: "Béja", distance: 200 },
+    { source: "Sousse", destination: "Kef", distance: 198 },
+    { source: "Sousse", destination: "Manouba", distance: 170 },
+    { source: "Sousse", destination: "Médenine", distance: 356 },
+    
     { source: "Kasserine", destination: "Ariana", distance: 259 },
-    { source: "Kasserine", destination: "Monastir", distance: 232 },
-    { source: "Kasserine", destination: "Sousse", distance: 196 },
-    { source: "Kasserine", destination: "Tunis", distance: 298 },
-    { source: "Kasserine", destination: "Sfax", distance: 199 },
-    { source: "Kasserine", destination: "Mahdia", distance: 242 },
+    { source: "Kasserine", destination: "Bizerte", distance: 367 },
+    { source: "Kasserine", destination: "Gabès", distance: 273 },
+    { source: "Kasserine", destination: "Gafsa", distance: 110 },
+    { source: "Kasserine", destination: "Jendouba", distance: 184 },
+    { source: "Kasserine", destination: "Kairouan", distance: 137 },
+    { source: "Kasserine", destination: "Kébili", distance: 222 },
+    { source: "Kasserine", destination: "Béja", distance: 231 },
+    { source: "Kasserine", destination: "Kef", distance: 129 },
+    { source: "Kasserine", destination: "Manouba", distance: 269 },
+    { source: "Kasserine", destination: "Médenine", distance: 346 },
+
     { source: "Monastir", destination: "Kef", distance: 253 },
     { source: "Monastir", destination: "Sidi Bouzid", distance: 175 },
     { source: "Monastir", destination: "Kébili", distance: 380 },
     { source: "Monastir", destination: "Ben Arous", distance: 161 },
-    { source: "Ariana", destination: "Béja", distance: 116 },
+    { source: "Monastir", destination: "Kébili", distance: 380 },
+    { source: "Monastir", destination: "Ariana", distance: 180 },
+    { source: "Monastir", destination: "Nabeul", distance: 129 },
+    { source: "Monastir", destination: "Bizerte", distance: 256 },
+    { source: "Monastir", destination: "Gabès", distance: 282 },
+    { source: "Monastir", destination: "Gafsa", distance: 274 },
+    { source: "Monastir", destination: "Jendouba", distance: 320 },
+    { source: "Monastir", destination: "Kairouan", distance: 68 },
+    { source: "Monastir", destination: "Kébili", distance: 421 },
+    { source: "Monastir", destination: "Béja", distance: 200 },
+    { source: "Monastir", destination: "Kef", distance: 198 },
+    { source: "Monastir", destination: "Manouba", distance: 170 },
+    { source: "Monastir", destination: "Médenine", distance: 356 },
+
+
+    { source: "Nabeul", destination: "Ariana", distance: 79 },
+    { source: "Nabeul", destination: "Zaghouan", distance: 64 },
+    { source: "Nabeul", destination: "Kef", distance: 231 },
+    { source: "Nabeul", destination: "Ben Arous", distance: 60 },
+    { source: "Nabeul", destination: "Gabès", distance: 376 },
+    { source: "Nabeul", destination: "Gafsa", distance: 324 },
+    { source: "Nabeul", destination: "Jendouba", distance: 219 },
+    { source: "Nabeul", destination: "Kairouan", distance: 122 },
+    { source: "Nabeul", destination: "Kébili", distance: 475 },
+    { source: "Nabeul", destination: "Béja", distance: 174 },
+    { source: "Nabeul", destination: "Bizerte", distance: 155 },
+    { source: "Nabeul", destination: "Manouba", distance: 77 },
+    { source: "Nabeul", destination: "Médenine", distance: 450 },
+    { source: "Nabeul", destination: "Sidi Bouzid", distance: 234 },
+    { source: "Nabeul", destination: "Siliana", distance: 181 },
+    { source: "Nabeul", destination: "Tataouine", distance: 503 },
+    { source: "Nabeul", destination: "Tozeur", distance: 415 },
+    { source: "Ariana", destination: "Béja", distance: 174 },
   ];
 
   const selectedDistance = distances.find(
     (item) =>
       (item.source === governorateAddressSource &&
 
-       
- item.destination === governorateAddressDestination) ||
+
+        item.destination === governorateAddressDestination) ||
       (item.source === governorateAddressDestination &&
         item.destination === governorateAddressSource)
   );
@@ -120,7 +219,6 @@ function MapInformation() {
             <Option value="Nabeul">Nabeul</Option>
             <Option value="Sfax">Sfax</Option>
             <Option value="Sousse">Sousse</Option>
-            <Option value="Kasserine"> Kasserine</Option>
             <Option value="Ben Arous">Ben Arous</Option>
             <Option value="Bizerte">Bizerte</Option>
             <Option value="Gabès">Gabès</Option>
@@ -168,7 +266,6 @@ function MapInformation() {
             <Option value="Gafsa"> Gafsa</Option>
             <Option value="Jendouba">Jendouba</Option>
             <Option value="Kairouan">Kairouan</Option>
-            <Option value="Kasserine"> Kasserine</Option>
             <Option value="Kébili">Kébili</Option>
             <Option value="Béja">Béja</Option>
             <Option value="Kef">Kef</Option>
